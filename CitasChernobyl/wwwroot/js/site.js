a8787c626fac20c91c768c96e66ebea6452c8844
francGit <fmuneton1@gmail.com>
2022-09-18 23:55:51 -0500

    se realizo los ajustes globales de estilos y js para posteriores animaciones, los estilos se trabajaran con sass

--- CitasChernobyl/wwwroot/js/site.js
+++ CitasChernobyl/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+$(document).ready(function(){
+    ////glish animation imagen logo
+
+
+
+    ////animacion index login
+    if ($(".boxTitular")[0]){
+        // Do something if class exists
+        console.log('si estoy');
+        gsap.fromTo(".boxTitular", {opacity: 0, top:0}, {opacity: 1, duration: 2});
+        gsap.to([$('.boxItem'), document.querySelector(".boxItem")], {opacity:1,top:0, stagger: 0.4});
+
+
+    } else {
+        // Do something if class does not exist
+        console.log('ya no estoy');
+    }
+});
